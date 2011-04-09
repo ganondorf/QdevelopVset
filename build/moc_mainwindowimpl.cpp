738a9aa8f64b71de267b86d3adf8f4dbe21e447b
Ganondorf <cagonzalez9@miners.utep.edu>
2011-04-09 16:08:46 -0600

    fixed commit conflicts?

--- build/moc_mainwindowimpl.cpp
+++ build/moc_mainwindowimpl.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindowimpl.h'
 **
-** Created: Sat Apr 9 12:35:52 2011
+** Created: Sat Apr 9 16:06:21 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.0)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,7 +23,7 @@ static const uint qt_meta_data_MainWindowImpl[] = {
        5,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -36,7 +36,9 @@ static const uint qt_meta_data_MainWindowImpl[] = {
       40,   15,   15,   15, 0x08,
       61,   15,   15,   15, 0x08,
       80,   15,   15,   15, 0x08,
-     104,   90,   15,   15, 0x08,
+      90,   15,   15,   15, 0x08,
+     100,   15,   15,   15, 0x08,
+     109,   15,   15,   15, 0x08,
 
        0        // eod
 };
@@ -44,7 +46,7 @@ static const uint qt_meta_data_MainWindowImpl[] = {
 static const char qt_meta_stringdata_MainWindowImpl[] = {
     "MainWindowImpl\0\0openSelect()\0saveFile()\0"
     "OpenAnimatorWindow()\0refreshTreeItems()\0"
-    "openVel()\0currentAction\0doAction(int)\0"
+    "openVel()\0doSlice()\0doZoom()\0doPan()\0"
 };
 
 const QMetaObject MainWindowImpl::staticMetaObject = {
@@ -83,10 +85,12 @@ int MainWindowImpl::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         case 2: OpenAnimatorWindow(); break;
         case 3: refreshTreeItems(); break;
         case 4: openVel(); break;
-        case 5: doAction((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: doSlice(); break;
+        case 6: doZoom(); break;
+        case 7: doPan(); break;
         default: ;
         }
-        _id -= 6;
+        _id -= 8;
     }
     return _id;
 }
